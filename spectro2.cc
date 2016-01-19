8879b49a51ad0f5ec36054157f8d3f52c5482e0b
name <jack.henry.lindon@cern.ch>
2016-01-19 20:38:01 +0000

    Changed the resolutions back into lab frame rather than beam frame, easier to interpret since the momentums are measured in lab frame.

--- spectro2.cc
+++ spectro2.cc
@@ -563,8 +563,6 @@ void spectro2::Process( int iEvent )
                         ////////////////////////////////////
                         // Calculate Momentum Resolutions //
                         ////////////////////////////////////
-                        true_momentum.RotateY(BeamAngleFromZAxis);
-                        momentum.RotateY(BeamAngleFromZAxis);
                         TVector3 ResolutionTemp = true_momentum - momentum;
                         FillHisto( "SpectrometerXMomentumResolution",  ResolutionTemp[0] );
                         FillHisto( "SpectrometerYMomentumResolution", ResolutionTemp[1] );
@@ -576,8 +574,6 @@ void spectro2::Process( int iEvent )
                         FillHisto( "SpectrometerZMomentumResolutionAgainstZMomentum", true_momentum[2] / 1000, ResolutionTemp[2] / 1000. );
                         FillHisto( "SpectrometerMomentumResolutionAgainstMomentum",  true_momentum.Mag() / 1000, ResolutionTemp.Mag() / 1000. );
 
-                        true_momentum.RotateY(-BeamAngleFromZAxis);
-                        momentum.RotateY(-BeamAngleFromZAxis);
 
                         true_momentum.RotateY(BeamAngleFromZAxis);
                         FillHisto( "KaonEndingPosition", true_position_end[2] / 1000., true_position_end[0] );
@@ -738,8 +734,8 @@ void spectro2::EndOfRunUser()
     for(int i=1;i<=15;i++)
     {
         x[i-1] = 5*i;
-        xx[i] = 0.03214285714*(i-1)-0.225;
-        xy[i] = 0.03214285714*(i-1)-0.225;
+        xx[i-1] = 0.03214285714*(i-1)-0.225;
+        xy[i-1] = 0.03214285714*(i-1)-0.225;
         xz[i-1] = 5*i;
 
         string intstr = to_string(i);
